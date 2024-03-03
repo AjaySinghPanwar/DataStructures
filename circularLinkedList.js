602115194afc6740f0bc65678704ff47db12cb06
Ajay Singh Panwar <64411148+AjaySinghPanwar@users.noreply.github.com>
2024-03-03 22:23:40 +0530

    Updated circularLinkedList.js
    Added function to delete a given node from the circular linked list

--- circularLinkedList.js
+++ circularLinkedList.js
@@ -88,15 +88,99 @@ function search(key){
     return false;
 }
 
+// Function to delete a node from the circular linked list
+function deleteNode(key){
+    if(head === null){
+        return;
+    }
+    else if(head.data === key && head.next === head){
+        head = null;
+    }
+    else if(head.data === key){
+        let last = head;
+        
+        while(last.next !== head){
+            last = last.next;
+        }
+        
+        last.next = head.next;
+        head = head.next;
+    }
+    else{
+        let current = head;
+        
+        while(current.next !== head){
+            if(current.next.data === key){
+                current.next = current.next.next;
+                break;
+            }
+            
+            current = current.next;
+        }
+    }
+}
+
 let head = null;
 
+console.log("Inserting at beginning .....")
 insertAtBeginning(100);
 insertAtBeginning(200);
 insertAtBeginning(300);
+traversal();
+console.log("\n")
+
+console.log("Inserting at end .....")
 insertAtEnd(400);
 insertAtEnd(500);
-insertAtBeginning(600);
-console.log(search(400));
-console.log(search(8999));
-
 traversal();
+console.log("\n")
+
+console.log("Inserting at beginning .....")
+insertAtBeginning(600);
+traversal();
+console.log("\n")
+
+console.log("Searching.....")
+console.log("400 found... ", search(400));
+console.log("8999 found... ", search(8999));
+console.log("\n");
+
+
+console.log("Traversing.....")
+traversal();
+console.log("\n");
+
+deleteNode(600)
+console.log("Deleting 600.....")
+traversal();
+console.log("\n");
+
+deleteNode(300);
+console.log("Deleting 300.....")
+traversal();
+console.log("\n");
+
+deleteNode(500);
+console.log("Deleting 500.....")
+traversal();
+console.log("\n");
+
+deleteNode(400);
+console.log("Deleting 400.....")
+traversal();
+console.log("\n");
+
+deleteNode(100);
+console.log("Deleting 100.....")
+traversal();
+console.log("\n");
+
+deleteNode(200);
+console.log("Deleting 200.....")
+traversal();
+console.log("\n");
+
+deleteNode(200);
+console.log("Deleting 200.....")
+traversal();
+console.log("\n");
