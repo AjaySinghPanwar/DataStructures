8bee74deaa5cc05a946421783e1609c3a76f0012
Ajay Singh Panwar <64411148+AjaySinghPanwar@users.noreply.github.com>
2024-03-05 00:40:16 +0530

    Updated stack.js
    Added implementation of stack using linked list

--- stack.js
+++ stack.js
@@ -75,3 +75,51 @@ console.log("Stack ===> ", arr, "\n");
 console.log("Popping....")
 pop();
 console.log("Stack ===> ", arr, "\n");
+
+
+
+// Stack using linked list
+class Node {
+    constructor(val){
+        this.data = val;
+        this.next = null;
+    }
+}
+
+let head = null;
+
+// Function to push data to stack
+function push(value){
+    let newNode = new Node(value);
+    
+    newNode.next = head;
+    head = newNode;
+}
+
+// Function for popping data from stack
+function pop(){
+    if(head === null){
+        console.log("Stack is empty!!!");
+    }
+    else{
+        console.log("Popped element... ", head.data);
+        head = head.next;
+    }
+}
+
+pop();
+
+console.log("Pushing... ", 45)
+push(45);
+
+console.log("Pushing... ", 90)
+push(90);
+
+console.log("Pushing... ", 890)
+push(890);
+
+pop();
+pop();
+pop();
+pop();
+
