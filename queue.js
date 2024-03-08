310a4e74bd413ddc40abfcda9ed296f82f953d08
Ajay Singh Panwar <64411148+AjaySinghPanwar@users.noreply.github.com>
2024-03-08 08:58:22 +0530

    Update queue.js
    Added Queue using linked list

--- queue.js
+++ queue.js
@@ -1,6 +1,6 @@
 // Queue (FIFO)
-// Using array
 
+// Using array
 const SIZE = 3, arr = [];
 let front = 0, rear = 0;
 
@@ -54,4 +54,75 @@ dequeue();
 dequeue();
 enqueue(66);
 enqueue(23);
+displayQueue();
+
+
+// Using linked list
+class Node {
+    constructor(value){
+        this.data = value;
+        this.next = null;
+    }
+}
+
+let front = null, rear = null;
+
+// Function to perform enqueue
+function enqueue(value){
+    const newNode = new Node(value);
+    newNode.next = null;
+    
+    if(front === null && rear === null){
+        front = rear = newNode;
+    }
+    else{
+        rear.next = newNode;
+        rear = newNode;
+    }
+}
+
+// Function to perform dequeue
+function dequeue(value){
+    if(front === null){
+        console.log("Queue is empty!!!");
+    }
+    else{
+        console.log("<=== Dequeued element ===>", front.data);
+        
+        front = front.next;
+        
+        if(front === null){
+            rear = null;
+        }
+    }
+}
+
+function displayQueue(){
+    if(front === null && rear === null){
+        return;
+    }
+    else{
+        let temp = front;
+        
+        console.group("<=== Queue elements ===>");
+        
+        while(temp !== null){
+            console.log(temp.data);
+            temp = temp.next;
+        }
+        
+        console.groupEnd();
+    }
+}
+
+dequeue();
+displayQueue();
+enqueue(1201);
+enqueue(1202);
+enqueue(1203);
+displayQueue();
+dequeue();
+dequeue();
+dequeue();
+dequeue();
 displayQueue();
