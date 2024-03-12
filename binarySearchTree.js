51cd8181c0ed144a6e7b928188baa90dde679158
Ajay Singh Panwar <64411148+AjaySinghPanwar@users.noreply.github.com>
2024-03-12 16:26:55 +0530

    Create binarySearchTree.js
    Added binary search tree

---
+++ binarySearchTree.js
@@ -0,0 +1,43 @@
+class BSTNode {
+    constructor(value){
+        this.key = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+// For inserting a node
+function insertNode(root, value){
+    if(root === null){
+        return new BSTNode(value);
+    }
+    else if(value < root.key){
+        root.left = insertNode(root.left, value);
+    }
+    else if(value > root.key){
+        root.right = insertNode(root.right, value);
+    }
+    
+    return root;
+}
+
+// For inorder traversal
+function inorderTraversal(root){
+    if(root === null){
+        return;
+    }
+    
+    inorderTraversal(root.left);
+    console.log(root.key);
+    inorderTraversal(root.right);
+}
+
+let root = null;
+console.log("Inserting node ", 100)
+root = insertNode(root, 100);
+console.log("Inserting node ", 200)
+root = insertNode(root, 200);
+console.log("Inserting node ", 300)
+root = insertNode(root, 300);
+
+inorderTraversal(root);
