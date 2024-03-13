75f3da639d348b73a6a95d07ce14b3e8bd5da8ac
Ajay Singh Panwar <64411148+AjaySinghPanwar@users.noreply.github.com>
2024-03-13 23:21:18 +0530

    Update binarySearchTree.js
    Added delte node function

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -32,7 +32,7 @@ function inorderTraversal(root){
     inorderTraversal(root.right);
 }
 
-// Function for searching a key
+// Function for searching a node
 function searchNode(root, value){
     if(root === null){
         return false;
@@ -48,6 +48,47 @@ function searchNode(root, value){
     }
 }
 
+// Function for deleting a node
+function getRightMin(root){
+    let temp = root;
+    
+    while(temp.left !== null){
+        temp = temp.left;
+    }
+    
+    return temp.key;
+}
+
+function deleteNode(root, value){
+    if(root === null){
+        return null;
+    }
+    else if(value < root.key){
+        root.left = deleteNode(root.left, value);
+    }
+    else if(value > root.key){
+        root.right = deleteNode(root.right, value);
+    }
+    else{
+        if(root.left === null && root.right === null){
+            return null;
+        }
+        else if(root.left === null){
+            return root.right;
+        }
+        else if(root.right === null){
+            return root.left;
+        }
+        else {
+            let rightMin = getRightMin(root.right);
+            root.key = rightMin;
+            root.right = removeNode(root.right, rightMin);
+        }
+    }
+    
+    return root;
+}
+
 let root = null;
 console.log("Inserting node ", 100)
 root = insertNode(root, 100);
@@ -55,6 +96,12 @@ console.log("Inserting node ", 200)
 root = insertNode(root, 200);
 console.log("Inserting node ", 300)
 root = insertNode(root, 300);
+console.log("Inserting node ", 600)
+root = insertNode(root, 600);
+console.log("Inserting node ", 400)
+root = insertNode(root, 400);
 
 inorderTraversal(root);
 console.log(searchNode(root, 500));
+root = deleteNode(root, 300);
+inorderTraversal(root);
