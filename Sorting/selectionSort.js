206f4a6780510e5f1f2092d2502839bc6c299778
Ajay Singh Panwar <64411148+AjaySinghPanwar@users.noreply.github.com>
2024-07-28 17:38:39 +0530

    Create selectionSort.js
    Added selection sort

---
+++ Sorting/selectionSort.js
@@ -0,0 +1,18 @@
+// Selection sort - Repeatedly select an element (smallest / largest) and move it to the sorted position
+// TC => O(n^2), SC => O(n)
+
+function selectionSort(arr, size){
+    let i, j;
+    
+    for(let i = 0; i < size - 1; ++i){
+        for(let j = i + 1; j < size; ++j){
+            if(arr[i] > arr[j]){
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+            }
+        }
+    }
+}
+
+let example_arr = [45, 6, 30, 2, 10];
+selectionSort(example_arr, 5);
+console.log(example_arr);
